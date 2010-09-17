0222074ee9e10a6c1af08e657b6bdb5e2c04e91f
askxuefeng <askxuefeng@localhost>
2010-09-17 22:12:57 +0800

    add comment

--- src/framework/validator.py
+++ src/framework/validator.py
@@ -22,21 +22,41 @@ def check_email(email):
     Raises:
         ValueError if not a valid email.
     '''
-    check_str(email, 'email')
+    check_str(email, 'email', False)
     if REGEX_EMAIL.match(email) is None:
         raise ValueError('%s is not an email' % email)
 
-def check_str(str, name='argument'):
+def check_str(str, name='argument', allow_empty=True):
     '''
     Check if it is a string.
     
     Args:
         str: object for test.
     Raises:
-        ValueError if not a string.
+        ValueError if not a string, or too long.
     '''
     if not isinstance(str, types.StringTypes):
         raise ValueError('%s is not a string' % name)
+    if not allow_empty and str=='':
+        raise ValueError('%s is empty' % name)
+    if len(str)>500:
+        raise ValueError('%s is too long (maximum to 500 characters)' % name)
+
+def check_text(text, allow_empty=True):
+    '''
+    Check if text is in range.
+    
+    Args:
+        text: a long str.
+    Raises:
+        ValueError if not a string, or too long.
+    '''
+    if not isinstance(str, types.StringTypes):
+        raise ValueError('not a text')
+    if not allow_empty and text=='':
+        raise ValueError('text is empty')
+    if len(text)>1048576:
+        raise ValueError('text is too long (maximum to 1048576 characters)')
 
 def check_password(password, allow_empty=True):
     '''
@@ -48,6 +68,6 @@ def check_password(password, allow_empty=True):
     Raises:
         ValueError if not a valid password.
     '''
-    check_str(password, 'password')
+    check_str(password, 'password', allow_empty)
     if (not allow_empty) and password and (REGEX_MD5.match(password) is None):
         raise ValueError('%s is not a password' % password)
