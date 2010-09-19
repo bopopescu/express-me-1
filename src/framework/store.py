607eb98c398f8554f4534fec26e6cd8fa55f29d9
askxuefeng <askxuefeng@localhost>
2010-09-19 17:00:17 +0800

    move counter to store module.

--- src/framework/store.py
+++ src/framework/store.py
@@ -7,8 +7,11 @@ __author__ = 'Michael Liao (askxuefeng@gmail.com)'
 All storage-related class and functions.
 '''
 
+import random
+
 from google.appengine.ext import db as db
 
+from framework import cache
 from framework import ValidationError
 from framework import validator
 
@@ -138,12 +141,78 @@ class User(BaseModel):
         '''
         return ROLE_NAMES[role//10]
 
-
-
+###############################################################################
+# Counter operation
 ###############################################################################
 
+CACHE_KEY_PREFIX = '_sharded_counter_'
 
+def get_count(name):
+    '''
+    Retrieve the value for a given sharded counter.
+    
+    Args:
+        name: the name of the counter
+    Returns:
+        Integer value
+    '''
+    total = cache.get(CACHE_KEY_PREFIX + name)
+    if total is None:
+        total = 0
+        for counter in ShardedCounter.all().filter('name =', name).fetch(1000):
+            total += counter.count
+        cache.set(CACHE_KEY_PREFIX + name, str(total))
+        return total
+    return int(total)
 
+def incr_count(name, delta=1):
+    '''
+    Increment the value for a given sharded counter.
+    
+    Args:
+        name: the name of the counter
+    '''
+    config = ShardedCounterConfig.get_or_insert(name, name=name)
+    def tx():
+        index = random.randint(0, config.shards-1)
+        shard_name = name + str(index)
+        counter = ShardedCounter.get_by_key_name(shard_name)
+        if counter is None:
+            counter = ShardedCounter(key_name=shard_name, name=name)
+        counter.count += delta
+        counter.put()
+    db.run_in_transaction(tx)
+    cache.incr(CACHE_KEY_PREFIX + name, delta=delta)
+
+def incr_counter_shards(name, num):
+    '''
+    Increase the number of shards for a given sharded counter.
+    Will never decrease the number of shards.
+    
+    Args:
+        name: the name of the counter
+        num: how many shards to use
+    '''
+    config = ShardedCounterConfig.get_or_insert(name, name=name)
+    def tx():
+        if config.shards < num:
+            config.shards = num
+            config.put()
+    db.run_in_transaction(tx)
+
+class ShardedCounterConfig(db.Model):
+    '''
+    Tracks the number of shards for each named counter.
+    '''
+    name = db.StringProperty(required=True)
+    shards = db.IntegerProperty(required=True, default=10)
+
+class ShardedCounter(db.Model):
+    '''
+    Shards for each named counter
+    '''
+    name = db.StringProperty(required=True)
+    count = db.IntegerProperty(required=True, default=0)
 
 ###############################################################################
 
